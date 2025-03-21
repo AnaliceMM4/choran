1bdb2ffdf144988ff7053fb761a7e8ce2d9cf5c3
Analice Moschen <62485958+AnaliceMM4@users.noreply.github.com>
2025-03-21 16:52:02 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,56 +1,31 @@
 import React, { useState } from "react";
 import './App.css'; // Importando o CSS
-import { FaPhoneAlt, FaUserAlt } from "react-icons/fa";
+import { FaInstagram, FaPhoneAlt, FaTelegramPlane, FaUserAlt, FaWhatsapp } from "react-icons/fa";
 import { PiBriefcaseFill } from "react-icons/pi";
 import { MdOutlineMail } from "react-icons/md";
-
-
-const CirclesWithDots = () => {
-  const words = [
-    "Escalamos empresas para múltiplos 5 dígitos/mês com previsibilidade.",
-    "Reduzimos o custo de aquisição em até X% enquanto aumentamos as conversões",
-    "Multiplicamos a recorrência e o ticket médio sem aumentar seu investimento em tráfego.",
-    "Eliminamos gargalos invisíveis que bloqueiam seu crescimento."
-  ];
-
-  return (
-    <div className="circle-container">
-      {[1, 2, 3, 4].map((i) => (
-        <div
-          key={i}
-          className="circle"
-          style={{
-            width: `${200 + i * 100}px`,
-            height: `${200 + i * 100}px`,
-            position: 'absolute',
-          }}
-        ></div>
-      ))}
-      {[{ top: "-20%", left: "85%" }, { top: "20%", left: "90%" }, { top: "50%", left: "90%" }, { top: "90%", left: "90%" }].map((pos, index) => (
-
-        // <div key={index} className="dot" style={{ top: pos.top, left: pos.left }}>
-        <div key={index} className="dot" style={{ position: 'absolute', top: pos.top, left: pos.left }}>
-
-          <span className="dot-label">{words[index]}</span>
-        </div>
-      ))}
-    </div>
-  );
-};
-
+import { BsFillRocketTakeoffFill } from "react-icons/bs";
+import { TbCircleCheckFilled } from "react-icons/tb";
 
 const Navbar = () => (
   <nav className="navbar">
     <div className="navList">
-
       <div>
-        <li className="logo"><a href="#home" className="navLink">Logo</a></li>
+        <li className="logo">
+          <a href="#logo" className="navLink">
+            <img src="logo.png" alt="" className="logo" />
+          </a>
+        </li>
       </div>
       <ul>
-
         <li><a href="#section1" className="navLink">Quero Vender Mais</a></li>
         <li><a href="#section2" className="navLink">Receber Informações</a></li>
         <li><a href="#section3" className="navLink">Agendar uma conversa</a></li>
+      </ul>
+
+      <ul>
+        <FaInstagram className="icon-redes" />
+        <FaWhatsapp className="icon-redes" />
+        <FaTelegramPlane className="icon-redes" />
       </ul>
     </div>
 
@@ -60,23 +35,23 @@ const Navbar = () => (
 // Componente Footer
 const Footer = () => (
   <footer className="footer">
-    <p>© 2025 Your Company. All rights reserved.</p>
+    <p>© 2025. Desenvolvido por Analice Moschen.</p>
   </footer>
 );
 
 // Componente principal App
 const App = () => {
-  const [form, setForm] = useState({});
-  const [errors, setErrors] = useState({});
-  const [activeSection, setActiveSection] = useState("section1");
-
-  const handleSectionChange = (section) => {
-    setActiveSection(section);
-  };
+  const [form, setForm] = useState({
+    nome: "",
+    email: "",
+    telefone: "",
+    segmento: "",
+  });
 
   const handleInputChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
+
   return (
     <div className="container">
       <Navbar />
@@ -86,28 +61,33 @@ const App = () => {
             <h2 className="">
               Fature de R$100 mil a R$500 mil em 6 meses com uma estrutura de marketing criada para sua necessidade.
             </h2>
-            <h5>Sistema de vendas previsível e escalável para sua empresa nunca parar de vender!</h5>
-            <p className="form-intro">Seja escalando o seu negócio ou criando um negócio digital do absoluto zero.    </p>
+            <h5>
+              <TbCircleCheckFilled className="icon-card" />
+              Sistema de vendas previsível e escalável para sua empresa nunca parar de vender!
+            </h5>
+            <h5>
+              <TbCircleCheckFilled className="icon-card" />
+              Seja escalando o seu negócio ou criando um negócio digital do absoluto zero.
+            </h5>
+
             <p className="form-intro">Tem um PROBLEMA. Nós RESOLVEMOS.   </p>
-           
-            <button type="submit" className="btn-vender">QUERO VENDER MAIS</button>
-          
+
+            <button type="submit" className="btn">
+              QUERO VENDER MAIS
+              <BsFillRocketTakeoffFill className="icon" />
+            </button>
           </div>
 
           <img src="section1.png" alt="Fundo da Seção 1" className="section1-image" />
-
-          {/* Se quiser ativar os círculos com textos, descomente a linha abaixo */}
-          {/* <CirclesWithDots /> */}
         </div>
       </section>
 
-
       <section className="section2" id="section2">
         <div className="section-content-image">
+          <img src="foguete-grafico.png" alt="a" className="section-image" />
           <div className="section-text">
             <h2 className="break-text">Soluções de marketing para os mais diversos negócios de diversos tamanhos.</h2>
             <h5 className="form-intro">PREENCHA O FORMULÁRIO </h5>
-
             <form className="register">
               <div className="input-row mb-2 ">
                 <div className="input-group" data-mdb-input-init>
@@ -119,6 +99,7 @@ const App = () => {
                     <input
                       type="nome"
                       id="nome"
+                      name="nome"
                       className="form-control"
                       value={form.nome}
                       onChange={handleInputChange}
@@ -126,16 +107,17 @@ const App = () => {
                   </div>
                 </div>
 
-                {/* <div className="row mb-2"> */}
-                <div className="" data-mdb-input-init>
+                <div className="">
                   <label className="form-label" htmlFor="email">E-mail</label>
                   <div className="input-container">
-                    <span className={`input-icon ${form.nome ? "hidden" : ""}`}>
+                    <span className={`input-icon ${form.email ? "hidden" : ""}`}>
                       <MdOutlineMail />
                     </span>
                     <input
                       type="email"
                       id="email"
+                      name="email"
+
                       className="form-control"
                       value={form.email}
                       onChange={handleInputChange}
@@ -154,6 +136,7 @@ const App = () => {
                     <input
                       type="telefone"
                       id="telefone"
+                      name="telefone"
                       className="form-control"
                       value={form.telefone}
                       onChange={handleInputChange}
@@ -170,6 +153,7 @@ const App = () => {
                     <input
                       type="segmento"
                       id="segmento"
+                      name="segmento"
                       className="form-control"
                       value={form.segmento}
                       onChange={handleInputChange}
@@ -178,17 +162,17 @@ const App = () => {
                 </div>
               </div>
 
-              <button type="submit" className="btn">RECEBER INFORMAÇÕES</button>
+              <button type="submit" className="btn">
+                RECEBER INFORMAÇÕES
+                <BsFillRocketTakeoffFill className="icon" />
+              </button>
             </form>
-
           </div>
-          {/* <img src="foguete1.jpg" alt="a" className="section-image" /> */}
-
         </div>
       </section>
 
       <section className="section3" id="section3">
-        <h2>O que a Choran entrega</h2>
+        <h2>O que a Choran entrega:</h2>
         <div className="card-cursos">
           <div className="card-content">
             <img src="escalar.png" alt="Imagem do curso" className="card-image" />
@@ -215,16 +199,19 @@ const App = () => {
             </div>
           </div>
         </div>
-        <p>
-          Se você está investindo e não está vendo retorno, não é um problema do digital.
 
-          É um problema de ESTRATÉGIA.
-        </p>
+        <div className="form-section3">
+          <h4> Se você está investindo e não está vendo retorno, não é um problema do digital. É um problema de ESTRATÉGIA. </h4>
+        </div>
+
+        <button type="submit" className="btn-submit">
+          AGENDAR UMA CONVERSA
+          <BsFillRocketTakeoffFill className="icon" />
+        </button>
 
       </section>
-
       <Footer />
-    </div>
+    </div >
   );
 };
 
